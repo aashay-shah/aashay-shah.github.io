2bd851970238f6f0280b6bfcd83165c3532a27bd
aashayshah160499 <aashay.shah@somaiya.edu>
2021-02-20 02:08:46 +0530

    Preloader Added

--- javascript/main.js
+++ javascript/main.js
@@ -141,3 +141,12 @@
     }
   });
 })();
+
+// Preloader
+
+window.addEventListener("load", () => {
+  document.querySelector(".preloader").classList.add("fade-out");
+  setTimeout(() => {
+    document.querySelector(".preloader").style.display = "none";
+  }, 600);
+});
